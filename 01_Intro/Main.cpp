eb301f7464b545c1f662166e51e26ea2ad79720a
Yvanlos <martinyvan.owono@yahoo.fr>
2025-05-14 11:18:12 +0200

    Initial commit on branch develliper

---
+++ 01_Intro/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//iostream : input, output, stream
+int main()
+{
+
+    std::cout <<"Hello World!" << std::endl;
+    // std::cout bdt console output
+    // std::endl bdt \n 
+
+     return 0; 
+     //success
+
+}
---
+++ 02_Basics/variables3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// was für ein Werte Bereich brauche ich in meine Variable
+int main()
+{
+    
+
+    return 0;
+}
